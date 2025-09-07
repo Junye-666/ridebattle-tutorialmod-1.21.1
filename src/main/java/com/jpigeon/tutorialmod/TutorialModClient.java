cf38e24470078e10887b36d15f1cb659f6b7bcf7
Junye <d_d5du@163.com>
2025-09-07 16:15:51 +0200

    Initial commit

---
+++ src/main/java/com/jpigeon/tutorialmod/TutorialModClient.java
@@ -0,0 +1,23 @@
+package com.jpigeon.tutorialmod;
+
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
+import net.neoforged.neoforge.client.gui.ConfigurationScreen;
+import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
+
+@Mod(value = TutorialMod.MODID, dist = Dist.CLIENT)
+@EventBusSubscriber(modid = TutorialMod.MODID, value = Dist.CLIENT)
+public class TutorialModClient {
+    public TutorialModClient(ModContainer container) {
+        container.registerExtensionPoint(IConfigScreenFactory.class, ConfigurationScreen::new);
+    }
+
+    @SubscribeEvent
+    static void onClientSetup(FMLClientSetupEvent event) {
+
+    }
+}
